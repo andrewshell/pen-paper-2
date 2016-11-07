22f1f50e0a53b33933e2911e7439346a034ec00d
Andrew Shell <andrew@andrewshell.org>
2016-11-07 10:07:07 -0600

    Adding AuraSession Middleware

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -2,8 +2,11 @@
 namespace PenPaper\Module;
 
 use Aura\Di\Container;
+use Aura\Session\SessionFactory;
 use Cadre\Module\Module;
 use PenPaper\Delivery\DefaultResponder;
+use Psr7Middlewares\Middleware\AttributeMapper;
+use Psr7Middlewares\Middleware\AuraSession;
 use Psr7Middlewares\Middleware\Robots;
 use Psr7Middlewares\Middleware\TrailingSlash;
 use Radar\Adr\Handler\RoutingHandler;
@@ -48,6 +51,16 @@ class Core extends Module
             'exceptionResponse' => $di->lazyNew(Response::class),
         ];
 
+        /** AuraSession */
+
+        $di->params[AuraSession::class] = [
+            'factory' => $di->lazyNew(SessionFactory::class),
+        ];
+
+        $di->setters[AuraSession::class] = [
+            'name' => 'pen-paper',
+        ];
+
         /** Robots */
 
         $di->params[Robots::class] = [
@@ -63,6 +76,14 @@ class Core extends Module
         $di->setters[TrailingSlash::class] = [
             'redirect' => 301,
         ];
+
+        /** AttributeMapper */
+
+        $di->params[AttributeMapper::class] = [
+            'mapping' => [
+                AuraSession::KEY => 'session',
+            ],
+        ];
     }
 
     public function modify(Container $di)
@@ -73,6 +94,8 @@ class Core extends Module
         $adr->middle(ExceptionHandler::class);
         $adr->middle(RoutingHandler::class);
         $adr->middle(ActionHandler::class);
+        $adr->middle(AttributeMapper::class);
+        $adr->middle(AuraSession::class);
         $adr->middle(TrailingSlash::class);
         $adr->middle(Robots::class);
 
